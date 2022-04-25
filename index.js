c97a642776a7af334cf35cca0d5dc8a61f6a6880
Joao Paulo <oliveirajpufrj@gmail.com>
2022-04-25 14:25:16 -0300

    finished w/ bonus

---
+++ index.js
@@ -0,0 +1,41 @@
+import express, { json } from "express";
+import cors from "cors";
+import chalk, { supportsColor } from "chalk";
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+let users = []
+let tweets = []
+let userAvatar;
+
+
+app.post("/sign-up", (req, res) => {
+  users.push(req.body);
+  userAvatar = req.body.avatar
+  res.send("OK")
+});
+
+app.get('/tweets', (req, res) => { 
+	res.send(tweets.slice(-10).reverse());
+});
+
+app.post("/tweets" , (req, res) => {
+  const data = req.body;
+
+	const tweetData = {
+		username: data.username,
+		avatar: userAvatar,
+		tweet: data.tweet
+	}
+
+	tweets.push(tweetData);
+    res.send("OK");
+	console.log("tweets", tweets);
+});
+
+
+app.listen(5000, () => {
+  console.log(chalk.bold.blue("Server is running on: http://localhost:5000"));
+});
